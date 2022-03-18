97201cd8a834d5174f222a6f68b968132f4b4abe
Bryan Jeff Saycon <bryanjeff.saycon@g.msuiit.edu.ph>
2022-03-18 15:06:29 +0800

    creating components

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import SearchWeather from "./components/SearchWeather";
+import './App.css';
+
+
+function App() {
+    return (
+        <div>
+            <SearchWeather />
+        </div>
+    )
+}
+
+export default App;
