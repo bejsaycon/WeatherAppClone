5fc16da2e9b352f5230798d56c61c842a7c779a9
Bryan Jeff Saycon <bryanjeff.saycon@g.msuiit.edu.ph>
2022-03-18 18:05:29 +0800

    final output done

--- src/components/SearchWeather.jsx
+++ src/components/SearchWeather.jsx
@@ -11,7 +11,6 @@ function SearchWeather() {
             const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=6c5c5cf22589babaf36d7394a4efbf42`)
             if (componentMounted){
                 setData(await response.json());
-                console.log(data);
             }
             return () => {
                 componentMounted = false;
@@ -19,19 +18,25 @@ function SearchWeather() {
         }
         
         fetchWeather();
-    }, [])
+    }, [search])
+    
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setSearch(input);
+    }
 
+    // Dynamic icons for the UI
     let emoji = null;
-    if(data.weather[0].main !== undefined){
-        if(data.weather[0].main ==="Clouds"){
+    if(typeof data.main != "undefined"){
+        if(data.weather[0].main =="Clouds"){
             emoji = "fa-cloud";
-        } else if(data.weather[0].main ==="Thunderstorm"){
+        } else if(data.weather[0].main =="Thunderstorm"){
             emoji = "fa-bolt";
-        } else if(data.weather[0].main ==="Drizzle"){    
+        } else if(data.weather[0].main =="Drizzle"){    
             emoji = "fa-cloud-drizzle";
-        } else if(data.weather[0].main ==="Rain"){
+        } else if(data.weather[0].main =="Rain"){
             emoji = "fa-cloud-showers-heavy";
-        } else if(data.weather[0].main ==="Snow"){
+        } else if(data.weather[0].main =="Snow"){
             emoji = "fa-snowflake";
         } else {
             emoji = "fa-smog";
@@ -47,6 +52,20 @@ function SearchWeather() {
     let temp_min = (data.main.temp_min - 273.15).toFixed(2);
     let temp_max = (data.main.temp_max - 273.15).toFixed(2);
 
+    //Date
+    let d = new Date();
+    let date = d.getDate();
+    let year = d.getFullYear();
+    let month = d.toLocaleString("default", {month:'long'});
+    let day = d.toLocaleString("default", {weekday:'long'});
+    //Time 
+    let time = d.toLocaleString([], {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit'
+    })
+
+    console.log(d,date,year,month,day);
   return (
     <div>
       <div className="container mt-5">
@@ -54,12 +73,12 @@ function SearchWeather() {
           <div className="col-md-4">
             <div class="card text-white text-center border-0">
               <img
-                src="https://images.unsplash.com/photo-1647449036501-1341aeb48d8b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=600&q=60"
+                src={`https://source.unsplash.com/600x900/?${data.weather[0].main}`}
                 class="card-img"
                 alt="..."
               />
               <div class="card-img-overlay">
-                <form>
+                <form onSubmit={handleSubmit}>
                   <div class="input-group mb-4 w-75 mx-auto">
                     <input
                       type="search"
@@ -67,16 +86,21 @@ function SearchWeather() {
                       placeholder="Search City"
                       aria-label="Search City"
                       aria-describedby="basic-addon2"
+                      name = "search"
+                      value ={input}
+                      onChange ={(e)=>setinput(e.target.value)}
+                      required
                     />
                     <button type="submit" class ="input-group-text" id="basic-addon2">
                        <i className="fas fa-search"></i>
                     </button>
                   </div>
                 </form>
-                <div className="bg-light bg-opacity-50 py-3">
+                <div className="bg-dark bg-opacity-50 py-3">
                 <h2 class="card-title">{data.name}</h2>
                 <p class="card-text lead">
-                  Thursday, March 18, 2022
+                  {day}, {month} {date}, {year} <br />
+                  {time}
                 </p>
                 <hr />
                 <i className={`fas ${emoji} fa-4x`}></i>
