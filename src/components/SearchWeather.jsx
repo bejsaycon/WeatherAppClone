edcf9dc69d5357e5a44838665d2e4e5760f56976
Bryan Jeff Saycon <bryanjeff.saycon@g.msuiit.edu.ph>
2022-03-18 15:30:45 +0800

    card image src

--- src/components/SearchWeather.jsx
+++ src/components/SearchWeather.jsx
@@ -7,7 +7,7 @@ function SearchWeather() {
                 <div className="row">
                     <div className="col-md-4">
                         <div class="card bg-dark text-white">
-                            <img src="./jellyfish.jpg" class="card-img" alt="..." />
+                            <img src="D:\react\reactjs\src\components\jellyfish.jpg" class="card-img" alt="..." />
                             <div class="card-img-overlay">
                                 <h5 class="card-title">Card title</h5>
                                 <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
