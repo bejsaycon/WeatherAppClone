c47effb157cf2837f2bd8c88b58c04925e39cf0e
Bryan Jeff Saycon <bryanjeff.saycon@g.msuiit.edu.ph>
2022-03-18 16:45:11 +0800

    fetch and use state practice

--- src/components/SearchWeather.jsx
+++ src/components/SearchWeather.jsx
@@ -1,24 +1,71 @@
-import React from 'react'
+import React, {useState, useEffect} from "react";
 
 function SearchWeather() {
-    return (
-        <div>
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-4">
-                        <div class="card bg-dark text-white">
-                            <img src="D:\react\reactjs\src\components\jellyfish.jpg" class="card-img" alt="..." />
-                            <div class="card-img-overlay">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                <p class="card-text">Last updated 3 mins ago</p>
-                            </div>
-                        </div>
-                    </div>
+    const [search, setSearch] = useState("new york");
+    const [data, setData] = useState([]);
+    const [input, setinput] = useState("");
+    let componentMounted = true;
+
+    useEffect(() => {
+        const fetchWeather = async () =>{
+            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=6c5c5cf22589babaf36d7394a4efbf42`)
+            if (componentMounted){
+                setData(await response.json());
+                console.log(data);
+            }
+            return () => {
+                componentMounted = false;
+            }
+        }
+        
+        fetchWeather();
+    }, [])
+    
+
+  return (
+    <div>
+      <div className="container mt-5">
+        <div className="row justify-content-center">
+          <div className="col-md-4">
+            <div class="card text-white text-center border-0">
+              <img
+                src="https://images.unsplash.com/photo-1647449036501-1341aeb48d8b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=600&q=60"
+                class="card-img"
+                alt="..."
+              />
+              <div class="card-img-overlay">
+                <form>
+                  <div class="input-group mb-4 w-75 mx-auto">
+                    <input
+                      type="search"
+                      class="form-control"
+                      placeholder="Search City"
+                      aria-label="Search City"
+                      aria-describedby="basic-addon2"
+                    />
+                    <button type="submit" class ="input-group-text" id="basic-addon2">
+                       <i className="fas fa-search"></i>
+                    </button>
+                  </div>
+                </form>
+                <div className="bg-light bg-opacity-50 py-3">
+                <h2 class="card-title">{data.name}</h2>
+                <p class="card-text lead">
+                  Thursday, March 18, 2022
+                </p>
+                <hr />
+                <i className="fas fa-cloud fa-4x"></i>
+                <h1 className="fw-bolder mb-5">33.06 &deg;C</h1>
+                <p className="lead fw-bolder mb-0">Cloud</p>
+                <p className="lead">30.01&deg;C | 35.23&deg;C</p>
                 </div>
+              </div>
             </div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default SearchWeather    
+export default SearchWeather;
